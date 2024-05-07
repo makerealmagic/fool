d165a8f979e6c334f1748d6541f438e7da2c31bd
makerealmagic <rackyrockstar2002@gmail.com>
2024-05-07 15:30:09 +0530

    updated

---
+++ index.js
@@ -0,0 +1,12 @@
+function raki() {
+    var main = document.querySelector(".main");
+    main.style.visibility = "visible";
+        
+}
+btn2.addEventListener('click', ()=>{
+    return raki();   
+})
+btn1.addEventListener('click', ()=>{
+    document.write("Very Good")
+})
+
